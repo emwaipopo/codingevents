2038850b19e9b3d538aa0df99a03c85a80a68c6d
emwaipopo <emmanuel_mwaipopo@cable.comcast.com>
2020-05-14 13:30:05 -0400

    Added contact email and attendees fields

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,14 +1,19 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
+
+    @AssertTrue(message="Registration must be required")
+    private boolean registrationRequired;
+
+    @Min(value = 1, message="Needs at least 1 attendee to create event")
+    @Max(value = 250, message="This venue has a maximum capacity of 250")
+    private int attendees;
 
     @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
@@ -18,6 +23,7 @@ public class Event {
     private String description;
 
     @NotBlank(message = "Venue is required.")
+    @NotNull
     @Size(max = 30, message = "Venue must be up to 30 characters long")
     private String venue;
 
@@ -25,12 +31,15 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail, String venue) {
+    public Event(String name, String description, String contactEmail, String venue, boolean registrationRequired, int attendees) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
         this.venue = venue;
+        this.registrationRequired = registrationRequired;
+        this.attendees = attendees;
+
     }
 
     public Event() {
@@ -68,6 +77,22 @@ public class Event {
 
     public void setVenue(String venue) {
         this.venue = venue;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
     }
 
     public int getId() {
